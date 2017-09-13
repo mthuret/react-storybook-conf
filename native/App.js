61f17c122317cbb74baa5d42711f1218da75f71a
Marie-Laure Thuret <marielaure.thuret@algolia.com>
2017-09-13 14:12:38 +0200

    clean up

--- native/App.js
+++ native/App.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Text from './MyText';
 import StorybookUI from './storybook';
-
-class App extends React.Component {
-  render() {
-    return <Text />;
-  }
-}
+import MyText from './MyText';
 
 module.exports = __DEV__ ? StorybookUI : App;
