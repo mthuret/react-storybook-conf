4a7e220b1e4dc13f1449112d4dc0b8d6a593974f
Marie-Laure Thuret <marielaure.thuret@algolia.com>
2017-09-13 14:08:57 +0200

    clean up

--- src/stories/Pagination.stories.js
+++ src/stories/Pagination.stories.js
@@ -5,9 +5,6 @@ import { linkTo } from '@storybook/addon-links';
 import Pagination from '../Pagination';
 import { withKnobs, boolean, number, array } from '@storybook/addon-knobs';
 import { orderBy } from 'lodash';
-import { specs, describe, it } from 'storybook-addon-specifications';
-import { mount } from 'enzyme';
-import expect from 'expect';
 import { withInfo } from '@storybook/addon-info';
 
 const stories = storiesOf('Widgets/Pagination', module);
@@ -24,38 +21,3 @@ stories.add('First page selected', () => {
     />
   );
 });
-
-stories.add(
-  'Last page selected',
-  withInfo('description')(() => {
-    const story = (
-      <Pagination
-        nbPages={10}
-        currentPage={10}
-        onClick={action('refine')}
-        pagesPadding={3}
-      />
-    );
-
-    specs(() =>
-      describe('Pagination', () => {
-        it('Clicking on the showFirst button should select the first page', () => {
-          const wrapper = mount(story);
-          const onClick = expect.createSpy();
-          wrapper.setProps({ onClick });
-
-          const showFirst = wrapper.find(
-            '.ais-Pagination__itemFirst .ais-Pagination__itemLink'
-          );
-          showFirst.simulate('click');
-
-          expect(onClick).toHaveBeenCalled();
-          expect(onClick).toHaveBeenCalledWith(1);
-          wrapper.unmount();
-        });
-      })
-    );
-
-    return story;
-  })
-);
